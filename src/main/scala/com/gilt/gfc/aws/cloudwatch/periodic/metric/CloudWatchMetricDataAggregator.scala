21cebafad6b5962a21584de51853c74c297cd030
Gregor Heine <gheine@gilt.com>
2018-01-25 20:31:31 +0000

    Reinstate old (deprecated) api for CloudWatchMetricDataAggregator

--- src/main/scala/com/gilt/gfc/aws/cloudwatch/periodic/metric/CloudWatchMetricDataAggregator.scala
+++ src/main/scala/com/gilt/gfc/aws/cloudwatch/periodic/metric/CloudWatchMetricDataAggregator.scala
@@ -40,4 +40,59 @@ object CloudWatchMetricDataAggregator {
   def builder(publisher: CloudWatchMetricsPublisher): CloudWatchMetricDataAggregatorBuilder = {
     CloudWatchMetricDataAggregatorBuilder().withPublisher(publisher)
   }
+
+
+  /** Constructs a builder (immutable) that collects all the necessary parameters as well
+    * as allows for a partial construction of CloudWatchMetricDataAggregator objects,
+    * with common groups of parameters shared by multiple aggregated metrics.
+    *
+    * N.B. fully constructed CloudWatchMetricDataAggregator instances start to collect
+    *      metrics immediately, so a global background task that dumps them to CW needs
+    *      to be started first, please call start() somewhere early in your app startup sequence.
+    *
+    * @return builder instance that you can start customizing
+    */
+  @deprecated("Use builder(CloudWatchMetricsPublisher)", "1.2.0")
+  def builder(): CloudWatchMetricDataAggregatorBuilder = {
+
+    // This is needed to make sure we consume what aggregated metrics start to produce, otherwise
+    // system will OOM.
+    publisher.map(CloudWatchMetricDataAggregatorBuilder().withPublisher(_)).getOrElse(
+      throw new AssertionError("Please call start() before building any aggregated metrics.")
+    )
+  }
+
+
+  /** Starts a background task that periodically dumps aggregated metric data to CW.
+    *
+    * @param interval how frequently to dump data to CW. This is intentionally different
+    *                 from the metric aggregation interval. E.g. you may be aggregating
+    *                 metrics for 1min but dump them to CW every 5min, thus taking advantage
+    *                 of larger batch size in API calls and reducing costs.
+    */
+  @deprecated("Use CloudWatchMetricsPublisher.start()", "1.2.0")
+  def start( interval: FiniteDuration
+           ): Unit = synchronized {
+    if (publisher.isEmpty) {
+      publisher = Some(CloudWatchMetricsPublisher.start(interval))
+    }
+  }
+
+
+  /** Stops background tasks. */
+  @deprecated("Use CloudWatchMetricsPublisher.stop()", "1.2.0")
+  def stop(): Unit = synchronized {
+    publisher.foreach(_.stop())
+  }
+
+
+  /** Completely shuts down, can not be restarted. */
+  @deprecated("Use CloudWatchMetricsPublisher.shutdown()", "1.2.0")
+  def shutdown(): Unit = synchronized {
+    publisher.foreach(_.shutdown())
+  }
+
+
+  @volatile private[this]
+  var publisher: Option[CloudWatchMetricsPublisher] = None
 }
